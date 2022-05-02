cfa7c1d8bf23844edf01facf9d909fecd4c00d3b
shaurya0101 <104727207+shaurya0101@users.noreply.github.com>
2022-05-02 03:36:58 -0700

    Add files via upload

---
+++ models.py
@@ -0,0 +1,8 @@
+def geeks_view(request):
+
+    from django.db import models
+
+    class Person(models.Model):
+      first_name = models.CharField(max_length=30)
+      last_name = models.CharField(max_length=30)
+
---
+++ views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def geeks_view(request):
+    return HttpResponse("dishank")
